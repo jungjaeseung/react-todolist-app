60a350d533aa4dfc64a0774bbe60e76af4ec9aec
3Harang <jungjaeseung9475@gmail.com>
2023-02-02 14:22:09 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  useEffect(() => console.log("call the api"), []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length >= 4) {
-      console.log("SEARCH FOR", keyword);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]);
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
+  };
+  useEffect(() => console.log(toDos), [toDos]);
   return (
-    <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>Welcome back! {counter}</h1>
-      <Button onClick={onClick} text={"click me"} />
+    <div className={styles.container}>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="write your to do..."
+        ></input>
+      </form>
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button
+              className={styles.deleteBtn}
+              onClick={() => deleteBtn(index)}
+            >
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
